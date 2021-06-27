7a4f4b125ab7d5dab11de930edf3a60145131ce0
Dan Mason <58603208+dancbm@users.noreply.github.com>
2021-06-27 18:41:34 +0100

    Add loading and error states for handling celestial data

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
     const [celestialData, setCelestialData] = useState();
 
-    const [server] = useState({ SERVER_HOST: "localhost", SERVER_PORT: 3001 });
+    const [serverHost] = useState("localhost");
+    const [serverPort] = useState(3001);
 
     useEffect(() => {
         const GetCelestialData = async () => {
             try {
-                const res = await fetch(
-                    `http://${server.SERVER_HOST}:${server.SERVER_PORT}`
-                );
+                const res = await fetch(`http://${serverHost}:${serverPort}`);
                 const json = await res.json();
                 await setCelestialData(json);
+                setLoading(false);
+                console.log("Check for recurring calls");
             } catch (e) {
-                console.log(e);
+                await setError(e);
+                setLoading(false);
+                console.log("Check for recurring calls");
             }
         };
         GetCelestialData();
-    }, [setCelestialData]);
+    }, [setCelestialData, serverHost, serverPort]);
+
     return (
         <div>
-            {celestialData !== undefined ? (
+            {loading ? (
+                <p>Forming the solar system</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
                 <ul>
                     {celestialData.map((object) => (
                         <li key={object.name}>{object.name}</li>
                     ))}
                 </ul>
-            ) : null}
+            )}
         </div>
     );
 };
