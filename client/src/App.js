ddac99646fd46f3c62fbdca99b292c3338868fe3
Dan Mason <58603208+dancbm@users.noreply.github.com>
2021-06-27 18:24:10 +0100

    Validate client/server communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,35 @@
-function App() {
-    return <div className="App"></div>;
-}
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+    const [celestialData, setCelestialData] = useState();
+
+    const [server] = useState({ SERVER_HOST: "localhost", SERVER_PORT: 3001 });
+
+    useEffect(() => {
+        const GetCelestialData = async () => {
+            try {
+                const res = await fetch(
+                    `http://${server.SERVER_HOST}:${server.SERVER_PORT}`
+                );
+                const json = await res.json();
+                await setCelestialData(json);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        GetCelestialData();
+    }, [setCelestialData]);
+    return (
+        <div>
+            {celestialData !== undefined ? (
+                <ul>
+                    {celestialData.map((object) => (
+                        <li key={object.name}>{object.name}</li>
+                    ))}
+                </ul>
+            ) : null}
+        </div>
+    );
+};
 
 export default App;
