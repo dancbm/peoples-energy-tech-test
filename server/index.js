4c80047c2c510ae837deb2b8a1bb232651219a6d
Dan Mason <58603208+dancbm@users.noreply.github.com>
2021-06-27 17:14:43 +0100

    Initialise basic server/index.js and nodemon start script

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const host = process.env.PRODUCTION ? process.env.SERVER_HOST : "localhost";
+const port = process.env.PRODUCTION ? process.env.SERVER_PORT : 3001;
+const clientHost = process.env.PRODUCTION
+    ? process.env.CLIENT_HOST
+    : "localhost";
+const clientPort = process.env.PRODUCTION ? process.env.CLIENT_PORT : 3000;
+
+app.use(cors({ origin: `http://${clientHost}:${clientPort}` }));
+
+app.listen(port, () => {
+    console.log(`Server started at ${host}:${port}`);
+});
